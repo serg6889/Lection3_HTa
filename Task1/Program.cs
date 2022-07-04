2698be44999bc4a2d20dc32cb5536bfc2a621c1e
Sergey68 <sesergey@yandex.ru>
2022-07-04 19:54:51 +1100

    Please enter the commit message for your changes. Lines starting

---
+++ Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// программa принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+Console.WriteLine("Input 5-digits number ");
+string number = Console.ReadLine();
+
+if(number[0] == number[4] && number[1] == number[3])
+{
+    Console.WriteLine("This number is polindrom");
+}
+else
+{
+    Console.WriteLine("This number is not polindrom");
+}
