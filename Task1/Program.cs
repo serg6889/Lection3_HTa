db08267c8b4ed1ca96bb8594908f185ad241ff52
Sergey68 <sesergey@yandex.ru>
2022-07-04 20:02:09 +1100

    File is updated with Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,5 +1,5 @@
 ﻿// программa принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
-Console.WriteLine("Input 5-digits number ");
+Console.WriteLine(" Input 5-digits number ");
 string number = Console.ReadLine();
 
 if(number[0] == number[4] && number[1] == number[3])
